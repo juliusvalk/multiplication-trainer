cddf4d8683c3c26b22382dbaac41dfde4266f0e3
juliusvalk <44950058+juliusvalk@users.noreply.github.com>
2025-03-18 19:26:23 +0200

    Delete js/main.js

--- js/main.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MultiplicationTrainer from "./MultiplicationTrainer.js";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<MultiplicationTrainer />);
